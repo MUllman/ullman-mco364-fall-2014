4a2750e926504167389e8895425b49a20e71a7d2
mullman <marianullman@aol.com>
2014-11-11 22:32:32 -0500

    commiting a finished paint and jetty server

---
+++ src/ullman/network/jetty/JettyServer.java
@@ -0,0 +1,12 @@
+package ullman.network.jetty;
+
+import org.eclipse.jetty.server.Server;
+
+public class JettyServer {
+	
+	public static void main(String [] args) throws Exception{
+		Server server = new Server(1891);
+		server.start();
+	}
+
+}
--- src/ullman/paint/Paint.java
+++ src/ullman/paint/Paint.java
@@ -1,29 +1,55 @@
 package ullman.paint;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 
-public class Paint extends JFrame{
+public class Paint extends JFrame implements ActionListener {
 
-	public Paint(){
-		setSize(800,600);
+	private Canvas canvas;
+
+	public Paint() {
+		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Paint");
 		setLocationRelativeTo(null);
-		
-		Canvas canvas = new Canvas();
+
+		canvas = new Canvas();
 		add(canvas, BorderLayout.CENTER);
-		
+
 		DrawListner listner = new DrawListner(canvas);
-		
+
+		JButton button = new JButton("color");
+		button.addActionListener(this);
 		canvas.addMouseMotionListener(listner);
+		canvas.addMouseWheelListener(new MouseWheelListener() {
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				int newStroke = (canvas.getStroke() + (e.getUnitsToScroll() * (-1)));
+				if(newStroke<1){
+					newStroke = 1;
+				}
+				canvas.setStroke(newStroke);
+			}
+		});
+		add(button, BorderLayout.SOUTH);
 	}
-	
-	public static void main(String[] args){
+
+	public void actionPerformed(ActionEvent e) {
+		Color initialcolor = Color.RED;
+		Color color = JColorChooser.showDialog(this, "Select a color", initialcolor);
+		canvas.setColor(color);
+	}
+
+	public static void main(String[] args) {
 		Paint paint = new Paint();
 		paint.setVisible(true);
 	}
-	
-}
 
+}
